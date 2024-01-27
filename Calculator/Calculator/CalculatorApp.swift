fb9bda44675c317bf680277469043e1c67027f40
youminki <108206432+youminki@users.noreply.github.com>
2024-01-27 17:49:08 +0900

    Version.1
    계산기 Ui 디자인 및 버튼기능 구현

---
+++ Calculator/Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by 유민기 on 2024/01/27.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
